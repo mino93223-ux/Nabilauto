401066e7248583d510355995b17896b5e564980a
mino93223-ux <mino93223@gmail.com>
2026-04-24 19:08:06 +0100

    Create layout.js for the Nabil Auto website

---
+++ layout.js
@@ -0,0 +1,15 @@
+// layout.js
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const Layout = ({ children }) => {
+    return (
+        <>
+            <Header />
+            <main>{children}</main>
+            <Footer />
+        </>
+    );
+};
+
+export default Layout;
